0aab7bdf3153d4aa44b28b90ca6f3729b928b78c
Franco Maidana <francomaidanaescabues@gmail.com>
2025-09-20 19:45:27 -0300

    pagina base

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Mobile - touch
+comparison.addEventListener('touchstart', (e) => {
+  isDragging = true;
+  moveSlider(e.touches[0]);
+  e.preventDefault(); // evita el scroll al tocar
+});
+
+document.addEventListener('touchmove', (e) => {
+  if (!isDragging) return;
+  moveSlider(e.touches[0]);
+  e.preventDefault(); // evita el scroll mientras arrastras
+});
+
